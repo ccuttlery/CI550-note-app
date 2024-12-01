fec27824d2df32e5695ffca5eda13a6ac9f3996f
ccuttlery <charliecuttler@gmail.com>
2024-12-01 16:54:06 +0000

    Setting things up
    + Second activity
    + ListView with onclicklistener to link to second activity

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -1,12 +1,21 @@
 package com.example.noteapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +28,26 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        final TextView textView = findViewById(R.id.textView);
+        Intent testActivity = new Intent(this, TestActivity.class);
+
+        ArrayList<String> notePaths = new ArrayList<String>();
+        notePaths.add("note1");
+        notePaths.add("note2");
+        notePaths.add("note3");
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, notePaths);
+
+
+        final ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener((parent, view, position, id) -> {
+            testActivity.putExtra("FILEPATH", notePaths.get(position));
+            startActivity(testActivity);
         });
     }
 }
---
+++ app/src/main/java/com/example/noteapp/TestActivity.java
@@ -0,0 +1,39 @@
+package com.example.noteapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_test);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        final TextView textView = findViewById(R.id.textView);
+        textView.setText(getIntent().getStringExtra("FILEPATH"));
+
+        Intent mainActivity = new Intent(this, MainActivity.class);
+
+        final Button button1 = findViewById(R.id.button1);
+        button1.setOnClickListener(v -> {
+            startActivity(mainActivity);
+        });
+    }
+
+
+}
